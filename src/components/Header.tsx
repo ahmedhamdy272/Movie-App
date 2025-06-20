d0abb186e3336fe1bd87915e870a68abf09ce2f1
KoKa <116951132+ahmedhamdy272@users.noreply.github.com>
2025-06-20 10:58:32 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,20 @@
-import React from "react";
-
 const Header = () => {
   return (
     <div>
       <div className="pattern" />
 
       <div className="wrapper">
-              <header>
-                  <img src="./hero.png" alt="Hero" />
+        <header>
+          <img src="./hero.png" alt="Hero" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
-              </header>
-              
-
-              
+        </header>
       </div>
     </div>
   );
 };
 
 export default Header;
+
