2ea1b9841c9439e47a472cebd6c6a3f00d02085d
KoKa <116951132+ahmedhamdy272@users.noreply.github.com>
2025-06-20 10:05:02 +0300

    Delete src/appwrite.d.ts

--- src/appwrite.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "./appwrite.js" {
-  export function getTrendingMovies(): Promise<any[]>;
-  export function updateSearchCount(
-    searchTerm: string,
-    movie: any
-  ): Promise<void>;
-}
