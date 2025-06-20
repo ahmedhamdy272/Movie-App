a1f2409d848b976e79279971342e1a30854be614
KoKa <116951132+ahmedhamdy272@users.noreply.github.com>
2025-06-20 11:06:36 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -74,15 +74,7 @@ const App = () => {
     }
   };
 
-  // const loadTrendingMovies = async () => {
-  //   try {
-  //     const movies = await getTrendingMovies();
-  //     console.log("Trending movies:", movies);
-  //     setTrendingMovies(movies || []);
-  //   } catch (error) {
-  //     console.error(`Error fetching trending movies: ${error}`);
-  //   }
-  // };
+
 
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
